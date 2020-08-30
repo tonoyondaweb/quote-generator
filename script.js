7fdc291f6b47fc73e61b20d8e87cf99de1a023c0
Tanay <iamtanaylodh@gmail.com>
2020-08-30 11:29:49 +0530

    DOM manipulation op added - Actually generates code now than being static

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 // Get Quote from API
 async function getQuote(){
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
+        quoteText.innerText = data.quoteText;
+        authorText.innerText = data.quoteAuthor;
     } catch (error) {
         getQuote();
     }
