92d4bd024e442eec031861e52915d4bcdd295b0d
Tanay <iamtanaylodh@gmail.com>
2020-08-30 11:14:08 +0530

    Initial setup with styling and fetch op

---
+++ script.js
@@ -0,0 +1,21 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+// Get Quote from API
+async function getQuote(){
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+    } catch (error) {
+        getQuote();
+    }
+}
+
+//On load
+getQuote();
